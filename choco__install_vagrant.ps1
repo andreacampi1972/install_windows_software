eb41a27c6b36480f98279fcc196877d389dc4aa6
Andrea Campi <andrea.campi1972@gmail.com>
2022-08-19 12:07:58 +0200

    Update comment and caffeine

--- choco__install_vagrant.ps1
+++ choco__install_vagrant.ps1
@@ -1,6 +1,7 @@
-#Installa choco
+#Install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
+#Install vagrant
 choco install vagrant virtualbox virtualbox-guest-additions-guest.install -y
 
 vagrant plugin install vagrant-disksize
--- choco_install_windows_software.ps1
+++ choco_install_windows_software.ps1
@@ -1,11 +1,12 @@
-#Installa choco
+#Install choco
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 choco upgrade chocolatey -y
 choco install putty -y
-choco install mobaxterm -y 
-choco install openvpn -y 
-choco install windirstat -y 
+choco install mobaxterm -y
+choco install openvpn -y
+choco install windirstat -y
 choco install keepass -y
 choco install authy-desktop -y
 choco install atom.install -y
+choco install caffeine -y
