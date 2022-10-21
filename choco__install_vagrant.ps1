8a0fd4a8cb18e54041207f95da1b99fc54d8f778
Andrea Campi <andrea.campi1972@gmail.com>
2022-10-21 17:34:19 +0200

    commit compatibility version of virtualbox and vagrant

--- choco__install_vagrant.ps1
+++ choco__install_vagrant.ps1
@@ -2,6 +2,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 
 #Install vagrant
-choco install vagrant virtualbox virtualbox-guest-additions-guest.install -y
+choco install vagrant -y
+choco install virtualbox --version 6.1.40 -y
+choco install virtualbox-guest-additions-guest.install --version 6.1.38 -y
 
 vagrant plugin install vagrant-disksize
