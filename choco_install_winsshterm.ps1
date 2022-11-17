e30e86e86250d3d1c454ddefc9cbb4c534d1beab
Andrea Campi <andrea.campi@aep-italia.it>
2022-11-17 11:35:52 +0100

    insert installation of winsshterm

---
+++ choco_install_winsshterm.ps1
@@ -0,0 +1,8 @@
+#Install choco
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
+#Install vagrant
+choco install winsshterm -y
+choco install vcxsrv -y
+choco install putty -y
+choco install winscp.install -y
