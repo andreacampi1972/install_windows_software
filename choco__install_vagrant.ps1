4dbbe9994b367249e7629133052995a0740c2afd
Andrea Campi <andrea.campi1972@gmail.com>
2023-05-10 15:44:34 +0200

    Update choco__install_vagrant.ps1

--- choco__install_vagrant.ps1
+++ choco__install_vagrant.ps1
@@ -4,9 +4,9 @@ iwr -useb https://chocolatey.org/install.ps1 | iex
 
 #Install vagrant
 choco install vagrant -y
-choco install virtualbox --version 6.1.40 -y
-choco install virtualbox-guest-additions-guest.install --version 6.1.38 -y
+choco install virtualbox  -y
+#choco install virtualbox-guest-additions-guest.install --version 6.1.38 -y
 
-vagrant plugin install vagrant-disksize
+#vagrant plugin install vagrant-disksize
 #automatically installs the host's VirtualBox Guest Additions
-vagrant plugin install vagrant-vbguest
+#vagrant plugin install vagrant-vbguest
